1d7deddc0e7cd7fefe19df79e0b5c3068c0e825c
serii <seriiburduja@gmail.com>
2024-06-06 20:14:16 +0300

    feat: todo header

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,21 @@
 <script setup lang="ts"></script>
 <template>
-  <div class="popup">Popup</div>
+  <div class="popup">
+    <header class="popup__header">Todo App</header>
+  </div>
 </template>
-<style>
+<style lang="scss">
 #app {
+  padding: 3.2rem;
   width: 580px;
   border-radius: 16px;
   border: 1px solid #ECECEC;
   box-shadow: 0 3.2rem 6.4rem 0 rgba(0, 0, 0, 0.05);
+}
+.popup {
+  &__header {
+    font-size: 2.4rem;
+    font-weight: bold;
+  }
 }
 </style>
