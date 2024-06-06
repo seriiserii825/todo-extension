d0779c8d5796779778575f209e0ada8fa288d92e
serii <seriiburduja@gmail.com>
2024-06-06 20:25:58 +0300

    feat: Search

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,29 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import Search from "./components/popups/Search.vue";
+</script>
 <template>
   <div class="popup">
     <header class="popup__header">Todo App</header>
+    <div class="popup__search">
+      <Search />
+    </div>
   </div>
 </template>
 <style lang="scss">
 #app {
   padding: 3.2rem;
-  width: 580px;
+  width: 380px;
   border-radius: 16px;
-  border: 1px solid #ECECEC;
+  border: 1px solid #ececec;
   box-shadow: 0 3.2rem 6.4rem 0 rgba(0, 0, 0, 0.05);
 }
 .popup {
   &__header {
+    margin-bottom: 1.6rem;
     font-size: 2.4rem;
     font-weight: bold;
+  }
+  &__search {
   }
 }
 </style>
---
+++ src/components/popups/Search.vue
@@ -0,0 +1,54 @@
+<script setup lang="ts">
+import { ref } from "vue";
+const search = ref("");
+</script>
+
+<template>
+  <div class="search">
+    <input
+      type="text"
+      placeholder="Enter todo..."
+      name="search"
+      v-model="search"
+    />
+    <span>Add new</span>
+  </div>
+</template>
+<style lang="scss">
+.search {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: 4rem;
+  border: 1px solid #ececec;
+  border-radius: 4px;
+  input {
+    flex: 1;
+    text-indent: 1rem;
+    height: 100%;
+    border: none;
+    &::placeholder {
+      text-indent: 1rem;
+      color: #b3b3b3;
+    }
+  }
+  span {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 0 1rem;
+    font-size: 1.6rem;
+    font-weight: 500;
+    height: 100%;
+    color: white;
+    background: var(--accent);
+    border: 1px solid var(--accent);
+    cursor: pointer;
+    transition: all .4s;
+    &:hover {
+      color: var(--accent);
+      background: white;
+    }
+  }
+}
+</style>
